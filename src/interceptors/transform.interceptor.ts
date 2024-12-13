e54921885d8f93b90e007abffe52b3bd42f6252e
thesunnatillo <sunnatillocoder@gmail.com>
2024-12-13 09:49:22 +0500

    transform interceptor

---
+++ src/interceptors/transform.interceptor.ts
@@ -0,0 +1,19 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    console.log('Interceptor ishga tushdi...');
+    return next.handle().pipe(map((data) => ({ data, success: true })));
+  }
+}
